9a1222b053a2400455113e3f53b2994c448b1873
Radu Ilina <ilinaraducristian@protonmail.com>
2022-01-02 20:42:21 +0200

    added authentication verification for the socket io gateway

--- src/socket-io/socket-io.gateway.ts
+++ src/socket-io/socket-io.gateway.ts
@@ -1,23 +1,41 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from "@nestjs/websockets";
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway, WsException } from "@nestjs/websockets";
 import { Socket } from "socket.io";
 import SocketIoEvents from "../dtos/SocketIoEvents";
 import { MembersService } from "../members/members.service";
 import { SocketIoService } from "./socket-io.service";
+import { Keycloak } from "keycloak-connect";
+import { KEYCLOAK_INSTANCE } from "nest-keycloak-connect";
+import { Inject } from "@nestjs/common";
 
 @WebSocketGateway()
 class SocketIoGateway implements OnGatewayConnection<Socket>, OnGatewayDisconnect<Socket> {
 
   constructor(
     private readonly membersService: MembersService,
-    private readonly socketIoService: SocketIoService
+    private readonly socketIoService: SocketIoService,
+    @Inject(KEYCLOAK_INSTANCE)
+    private keycloak: Keycloak
   ) {
   }
 
   async handleConnection(client: Socket, ...args: any[]) {
-    // TODO verify token
-    return;
-    const token = client.handshake.auth.token;
-    const userId = client.handshake.auth.userId;
+    const jwt = client.handshake.auth.token;
+    let userId: string;
+
+    if (jwt === undefined || jwt === null) {
+      throw new WsException("unauthorized");
+    }
+
+    try {
+      const grant = await this.keycloak.grantManager.createGrant({ access_token: jwt });
+      if ((await this.keycloak.grantManager.validateAccessToken(grant.access_token)) === grant.access_token) {
+        const user = JSON.parse(Buffer.from(jwt.split(".")[1], "base64").toString());
+        userId = client.handshake.auth.userId = user.sub;
+      }
+    } catch (e) {
+      console.error(e);
+      throw new WsException("unauthorized");
+    }
     const servers = await this.membersService.getServers(userId);
     const serverIds = [];
     servers.forEach(server => {
@@ -28,9 +46,8 @@ class SocketIoGateway implements OnGatewayConnection<Socket>, OnGatewayDisconnec
     this.socketIoService.addSocketUserPair({ userId, socketId: client.id });
   }
 
+
   async handleDisconnect(client: Socket) {
-    // TODO get user id from auth
-    return;
     const userId = client.handshake.auth.userId;
     const servers = await this.membersService.getServers(userId);
     servers.forEach(server => {
